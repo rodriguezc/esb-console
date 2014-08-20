954b522d148c3581ab988080760be9030d5c0fe9
rodriguezc <chris@iperlon.com>
2014-08-20 13:28:30 +0200

    RW can also do operations RO

--- app/security/SecurityROChecker.java
+++ app/security/SecurityROChecker.java
@@ -7,6 +7,6 @@ public class SecurityROChecker extends SecurityChecker {
 
     @Override
     boolean validate(RightType right) {
-        return RightType.RO.equals(right);
+        return RightType.RO.equals(right) || RightType.RW.equals(right) ;
     }
 }
